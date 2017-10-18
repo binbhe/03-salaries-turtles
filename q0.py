518f80d4059785d3b5d7b5eb36aa96a4c94d94cc
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-17 23:59:02 -0500

    Update q0.py

--- q0.py
+++ q0.py
@@ -6,3 +6,17 @@ vec = get_data("salaries.csv")
 
 solution = "DIMAGGIO" # LAST NAME IN CAPS.
 
+def job():
+    first = 0
+    for job, annual_salary_money in zip(job_title, annual_salary):
+        if annual_salary_money != '':
+            money = float(annual_salary_money[1:])
+            if money > first:
+                first = money
+
+    for job, annual_salary_money in zip(job_title, annual_salary):
+        if annual_salary_money != '' and annual_salary_money == ('$' + str(first) + '0'):
+            print('MostSalaries')
+            print(job.strip('/"').strip(), annual_salary_money)
+            print()
+            break
