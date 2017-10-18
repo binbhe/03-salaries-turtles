e21b123415639d5c8903c9337bb6e898744efbb0
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-18 00:01:39 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -6,3 +6,15 @@ vec = get_data("salaries.csv")
 
 solution = 1234 # Part-time as an int.
 
+def part_time():
+    result = 0
+    for fp in salary_hourly:
+        if fp == 'P':
+            result += 1
+    if result == (employee_number() - full_time()):
+        print('第三题')
+        print('ok')
+        print(result)
+        print()
+    else:
+        print('no')
