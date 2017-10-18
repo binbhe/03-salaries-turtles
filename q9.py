3d5759025047cc9deaec73403e62e6463ca3c536
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-18 00:05:53 -0500

    Update q9.py

--- q9.py
+++ q9.py
@@ -8,3 +8,18 @@ vec = get_data("salaries.csv")
 # police officer name with 37 occurrences.
 solution = "JULIETTE"
 
+def girl_name_police():
+    count_name_set = set([])
+    count_name_list = []
+    for count_name, count_departement, count_full_part_time in zip(name, department, full_part_time):
+        if 'detective' in count_departement.lower():
+            count_name_list.append(count_name)
+            count_name_set.add(count_name)
+
+    for c_s in count_name_set:
+        count = name.count(c_s)
+        if count == 37:
+            print('Womencommonname')
+            print(c_s.strip('\"').strip())
+            print()
+            break
