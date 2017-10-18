a93bfdbb314a5a73757163ee223d0bb48115c80b
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-18 00:02:28 -0500

    Update q4.py

--- q4.py
+++ q4.py
@@ -6,3 +6,13 @@ vec = get_data("salaries.csv")
 
 solution = 67.89 # Highest hourly as a float.
 
+def hourly_rate_money():
+    max_money = 0
+    for money in hourly_rate:
+        if money != '':
+            money = float(money[1:])
+            if max_money < money:
+                max_money = money
+    print('highesthourlywage')
+    print(max_money)
+    print()
