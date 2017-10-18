4ef4204e458bcd65157fe5b3bec46ed3ed13f321
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-17 23:59:48 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -6,3 +6,11 @@ vec = get_data("salaries.csv")
 
 solution = 123 # Total mployees as int.
 
+def employee_number():
+    result = 0
+    for num in name:
+        result += 1
+    print('第一题')
+    print(result)
+    print()
+    return result
