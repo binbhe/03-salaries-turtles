532eb5b3a95a029cb1f84d9295e1a4616be27700
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-18 00:00:57 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -6,3 +6,12 @@ vec = get_data("salaries.csv")
 
 solution = 11111 # Full time works as an int.
 
+def full_time():
+    result = 0
+    for fp in salary_hourly:
+        if fp == 'F':
+            result += 1
+    print('full-time')
+    print(result)
+    print()
+    return result
