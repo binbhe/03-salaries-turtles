eb26e696994addf2e77971dd397f619b337d6d1e
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-18 00:03:40 -0500

    Update q6.py

--- q6.py
+++ q6.py
@@ -6,3 +6,11 @@ vec = get_data("salaries.csv")
 
 solution = 11223 # Police workers as an int.
 
+def police_number():
+    number = 0
+    for n in department:
+        if 'police' in n.lower():
+            number += 1
+    print(' policedepartment')
+    print(number)
+    print()
