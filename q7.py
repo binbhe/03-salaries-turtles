a4767bbc07347ca85fddaa92040479a76cf35d6a
Erin Binbin He <32334923+binbhe@users.noreply.github.com>
2017-10-18 00:04:21 -0500

    7

--- q7.py
+++ q7.py
@@ -6,3 +6,11 @@ vec = get_data("salaries.csv")
 
 solution = 1123 # Detectives as int.
 
+def detectives_number():
+    number = 0
+    for n in department:
+        if 'detective' in n.lower():
+            number += 1
+    print('detectives')
+    print(number)
+    print()
